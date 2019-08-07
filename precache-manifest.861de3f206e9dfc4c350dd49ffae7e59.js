a87db8911ff866cb1c2b0e3ec3235feb02b2a427
Iris Ji <iris.ji@e-ci.com.au>
2019-08-07 12:53:00 +1000

    Updates

---
+++ precache-manifest.861de3f206e9dfc4c350dd49ffae7e59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1972052628a10f82387133c5be30086e",
+    "url": "/seasons/index.html"
+  },
+  {
+    "revision": "a55c45e07ff439fc374d",
+    "url": "/seasons/static/css/main.cbf9779e.chunk.css"
+  },
+  {
+    "revision": "607320d00d64b68e18da",
+    "url": "/seasons/static/js/2.2050718e.chunk.js"
+  },
+  {
+    "revision": "a55c45e07ff439fc374d",
+    "url": "/seasons/static/js/main.28807b33.chunk.js"
+  },
+  {
+    "revision": "808fed5ac1e88b432622",
+    "url": "/seasons/static/js/runtime~main.fbc5b39b.js"
+  }
+]);
